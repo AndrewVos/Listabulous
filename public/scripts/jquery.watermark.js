f95e6c6cb7c4ad92ed25786e2961aa963d51b87a
AndrewVos <andrew.vos@gmail.com>
2010-08-30 12:14:10 +0100

    Readded public files

---
+++ public/scripts/jquery.watermark.js
@@ -0,0 +1,33 @@
+﻿(function ($) {
+    jQuery.fn.watermark = function (watermarkText, watermarkStyle) {
+        return this.each(function () {
+            var textBox = $(this);
+
+            if (textBox.val() == "") {
+                textBox.val(watermarkText);
+
+                if (watermarkStyle) {
+                    textBox.addClass(watermarkStyle);
+                }
+            }
+
+            textBox.focus(function () {
+                if (textBox.val() == watermarkText) {
+                    textBox.val("");
+                    if (watermarkStyle) {
+                        textBox.removeClass(watermarkStyle);
+                    }
+                }
+            });
+
+            textBox.blur(function () {
+                if (textBox.val() == "") {
+                    textBox.val(watermarkText);
+                    if (watermarkStyle) {
+                        textBox.addClass(watermarkStyle);
+                    }
+                }
+            });
+        });
+    };
+})(jQuery);
---
+++ public/scripts/listabulous.common.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function() {
+    initializeLogo();
+});
+
+function initializeLogo() {
+    var logoImage = $("div#logo a img");
+    var extension = ".png";
+    var extensionWithHover = "hover.png";
+
+    var preloader = new Image();
+    preloader.src = logoImage.attr("src").replace(extension, extensionWithHover);
+
+    logoImage.hover(function() {
+        $(this).attr("src", $(this).attr("src").replace(extension, extensionWithHover));
+    }, function() {
+        $(this).attr("src", $(this).attr("src").replace(extensionWithHover, extension));
+    });
+}
