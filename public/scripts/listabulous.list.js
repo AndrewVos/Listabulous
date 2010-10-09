26ca3004713d699a2c9d8c18a4b750d8581f970e
AndrewVos <andrew.vos@gmail.com>
2010-10-09 15:29:09 +0100

    Cleaned up js a bit more.

--- public/scripts/listabulous.list.js
+++ public/scripts/listabulous.list.js
@@ -1,171 +1,223 @@
 ﻿$(document).ready(function()
-{
-	List.initialize();
+ {
+    List.initialize();
 });
 
 var List = {
-	initialize : function()
-	{
-		List.listItemContainer = $("#ListItemContainer");
-		List.listItemEntry = $("#ListItemEntry");
-		List.colourPicker = $("#ColourPicker");
-		
-		List.initializeListItemEntryTextBox();
-		List.initializeEvents();
-		$(".ListItem").linkify();
-		List.sortItems();
-		List.updateWindowTitle();
-	},
-		
-	initializeListItemEntryTextBox : function ()
-	{
-		List.listItemEntry.watermark("« add a new task", "Prompt");
+    initialize: function()
+    {
+        List.listItemContainer = $("#ListItemContainer");
+        List.listItemEntry = $("#ListItemEntry");
+        List.colourPicker = $("#ColourPicker");
 
-		List.listItemEntry.keyup(function(event) {
-			if (event.keyCode == 13 || event.keyCode == 10) {
-				if (List.listItemEntry.val() != "") {
-					$.post("/api/add-list-item", { "text": List.listItemEntry.val(), "colour": List.getDefaultColour() }, function (response) {
-						List.listItemEntry.removeClass("Loading");
+        List.initializeListItemEntryTextBox();
+        List.initializeEvents();
+        $(".ListItem").linkify();
+        List.sortItems();
+        List.updateWindowTitle();
+    },
 
-						if (response)
-						{
-							var listItem = $(response);
-							listItem.linkify();
-							List.listItemContainer.append(listItem);
-							List.sortItems();
-							listItem.fadeIn();
-							List.updateWindowTitle();
-						}
-					});
+    initializeListItemEntryTextBox: function()
+    {
+        List.listItemEntry.watermark("« add a new task", "Prompt");
 
-					List.listItemEntry.addClass("Loading");
-					List.listItemEntry.val("");
-				}
-			}
-		});		
-	},
-	
-	getDefaultColour : function()
-	{
-		return $("#ChooseDefaultColour").css("background-color");
-	},
-	
-	initializeEvents : function()
-	{
-		$(document).click(function(event) {
-			var originalTarget = $(event.originalTarget)
+        List.listItemEntry.keyup(function(event) {
+            if (event.keyCode == 13 || event.keyCode == 10) {
+                if (List.listItemEntry.val() != "") {
+                    $.post("/api/add-list-item", {
+                        "text": List.listItemEntry.val(),
+                        "colour": List.getDefaultColour()
+                    },
+                    function(response) {
+                        List.listItemEntry.removeClass("Loading");
 
-			if (originalTarget.is("#ChooseDefaultColour, .ChooseListItemColour") == false)
-			{
-				List.colourPicker.fadeOut()
-			}
-		});
+                        if (response)
+                        {
+                            var listItem = $(response);
+                            listItem.linkify();
+                            List.listItemContainer.append(listItem);
+                            List.sortItems();
+                            listItem.fadeIn();
+                            List.updateWindowTitle();
+                        }
+                    });
 
-		$("#ChooseDefaultColour").live("click", function() {
-			var sender = $(this);
-			var newOffset = sender.offset();
-			newOffset.left = newOffset.left + sender.width();
-			List.showColourPicker(newOffset.left, newOffset.top)		
-		});
+                    List.listItemEntry.addClass("Loading");
+                    List.listItemEntry.val("");
+                }
+            }
+        });
+    },
 
-		$(".ListItemTitle").live("click", function() {
-			var sender = $(this);		
-			var listItem = sender.parent();
-			listItem.toggleClass("Complete");
+    getDefaultColour: function()
+    {
+        return $("#ChooseDefaultColour").css("background-color");
+    },
 
-			var id = List.getListItemId(listItem);
-			var complete = listItem.hasClass("Complete");
+    initializeEvents: function()
+    {
+        $(document).click(function(event) {
+            List.document_Click($(this), event);
+        });
 
-			$.post("/api/mark-list-item-complete", { "id": id, "complete": complete }, null, "json");
-			List.updateWindowTitle();
-		});
+        $("#ChooseDefaultColour").live("click",
+        function() {
+            List.chooseDefaultColour_Click($(this));
+        });
 
-		$(".ChooseListItemColour").live("click", function() {
-			var sender = $(this);
-			var listItem = sender.parent();
-			var newOffset = sender.offset();
-			newOffset.left = newOffset.left + sender.width();
-			List.showColourPicker(newOffset.left, newOffset.top, listItem)
-		});
+        $(".ListItemTitle").live("click",
+        function() {
+            List.listItemTitle_Click($(this));
+        });
 
-		$(".SelectColour").live("click", function() {
-			var sender = $(this);
-			var colour = sender.css("background-color");
-			var selectedListItem = List.colourPicker.data("selectedListItem");
+        $(".ChooseListItemColour").live("click",
+        function() {
+            List.chooseListItemColour_Click($(this));
+        });
 
-			if (selectedListItem == null)
-			{
-				$("#ChooseDefaultColour").css("background-color", colour);
-				var listItemEntry = $("#ListItemEntry");
-				listItemEntry.focus();
-				$.post("/api/set-user-default-colour", { "default_colour": colour }, null, "json");
-			}
-			else
-			{
-				selectedListItem.find(".ChooseListItemColour").css("background-color", colour);
-				List.sortItems();
-				$.post("/api/set-list-item-colour", { "id": List.getListItemId(selectedListItem), "colour": colour.toString() }, null, "json");
-			}
-		});
+        $(".SelectColour").live("click",
+        function() {
+            List.selectColour_Click($(this));
+        });
 
-		$(".DeleteListItem").live("click", function() {
-			var sender = $(this);
-			var listItem = sender.parent();		
-			var id = List.getListItemId(listItem);
+        $(".DeleteListItem").live("click",
+        function() {
+            List.deleteListItem_Click($(this));
+        });
+    },
 
-			listItem.remove();	
-			List.updateWindowTitle();		
-			$.post("/api/delete-list-item", { "id": id }, null, "json");		
-		});
-	},
-	
-	getListItemId : function(listItem)
-	{
-		return listItem.find(".ListItemId").attr("value");
-	},
-	
-	showColourPicker : function(x, y, selectedListItem)
-	{
-		if (selectedListItem === undefined) selectedListItem = null
 
-		List.colourPicker.data("selectedListItem", selectedListItem);
-		List.colourPicker.css({ left: x, top: y });
-		List.colourPicker.hide();
-		List.colourPicker.fadeIn();	
-	},
-	
-	sortItems : function()
-	{
-		var listItems = List.listItemContainer.children(".ListItem").get();
+    document_Click: function(sender, event)
+    {
+        var originalTarget = $(event.target);
 
-		listItems.sort(function (a, b)
-		{
-			var compA = $(a).find(".ListItemTitle").text().toUpperCase();
-			var compB = $(b).find(".ListItemTitle").text().toUpperCase();
-			return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-		});
-		listItems.sort(function (a, b)
-		{
-			var compA = $(a).find(".ChooseListItemColour").css("background-color");
-			var compB = $(b).find(".ChooseListItemColour").css("background-color");
-			return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
-		});
+        if (originalTarget == null || originalTarget.is("#ChooseDefaultColour, .ChooseListItemColour") == false)
+        {
+            this.colourPicker.fadeOut()
+        }
+    },
 
-		List.listItemContainer.append(listItems);
-	},
-	
-	updateWindowTitle : function()
-	{
-		var listItemCount = $(".ListItem").length;
-		var completeItemCount = $(".ListItem.Complete").length;
-		var incompleteItemCount = listItemCount - completeItemCount;
-		if (incompleteItemCount > 0)
-		{
-			document.title = $.format("{0} item{1} - Listabulous", incompleteItemCount, listItemCount == 1 ? "" : "s");
-		}
-		else
-		{
-			document.title = "Listabulous";
-		}	
-	}
+    chooseDefaultColour_Click: function(sender)
+    {
+        var newOffset = sender.offset();
+        newOffset.left = newOffset.left + sender.width();
+        this.showColourPicker(newOffset.left, newOffset.top)
+    },
+
+    listItemTitle_Click: function(sender)
+    {
+        var listItem = sender.parent();
+        listItem.toggleClass("Complete");
+
+        var id = this.getListItemId(listItem);
+        var complete = listItem.hasClass("Complete");
+
+        $.post("/api/mark-list-item-complete", {
+            "id": id,
+            "complete": complete
+        },
+        null, "json");
+        this.updateWindowTitle();
+    },
+
+    chooseListItemColour_Click: function(sender)
+    {
+        var listItem = sender.parent();
+        var newOffset = sender.offset();
+        newOffset.left = newOffset.left + sender.width();
+        this.showColourPicker(newOffset.left, newOffset.top, listItem)
+    },
+
+    selectColour_Click: function(sender)
+    {
+        var colour = sender.css("background-color");
+        var selectedListItem = this.colourPicker.data("selectedListItem");
+
+        if (selectedListItem == null)
+        {
+            $("#ChooseDefaultColour").css("background-color", colour);
+            var listItemEntry = $("#ListItemEntry");
+            listItemEntry.focus();
+            $.post("/api/set-user-default-colour", {
+                "default_colour": colour
+            },
+            null, "json");
+        }
+        else
+        {
+            selectedListItem.find(".ChooseListItemColour").css("background-color", colour);
+            this.sortItems();
+            $.post("/api/set-list-item-colour", {
+                "id": this.getListItemId(selectedListItem),
+                "colour": colour.toString()
+            },
+            null, "json");
+        }
+    },
+
+    deleteListItem_Click: function(sender)
+    {
+        var listItem = sender.parent();
+        var id = this.getListItemId(listItem);
+
+        listItem.remove();
+        this.updateWindowTitle();
+        $.post("/api/delete-list-item", {
+            "id": id
+        },
+        null, "json");
+    },
+
+    getListItemId: function(listItem)
+    {
+        return listItem.find(".ListItemId").attr("value");
+    },
+
+    showColourPicker: function(x, y, selectedListItem)
+    {
+        if (selectedListItem === undefined) selectedListItem = null
+
+        this.colourPicker.data("selectedListItem", selectedListItem);
+        this.colourPicker.css({
+            left: x,
+            top: y
+        });
+        this.colourPicker.hide();
+        this.colourPicker.fadeIn();
+    },
+
+    sortItems: function()
+    {
+        var listItems = this.listItemContainer.children(".ListItem").get();
+
+        listItems.sort(function(a, b)
+        {
+            var compA = $(a).find(".ListItemTitle").text().toUpperCase();
+            var compB = $(b).find(".ListItemTitle").text().toUpperCase();
+            return (compA < compB) ? -1: (compA > compB) ? 1: 0;
+        });
+        listItems.sort(function(a, b)
+        {
+            var compA = $(a).find(".ChooseListItemColour").css("background-color");
+            var compB = $(b).find(".ChooseListItemColour").css("background-color");
+            return (compA < compB) ? -1: (compA > compB) ? 1: 0;
+        });
+
+        this.listItemContainer.append(listItems);
+    },
+
+    updateWindowTitle: function()
+    {
+        var listItemCount = $(".ListItem").length;
+        var completeItemCount = $(".ListItem.Complete").length;
+        var incompleteItemCount = listItemCount - completeItemCount;
+        if (incompleteItemCount > 0)
+        {
+            document.title = $.format("{0} item{1} - Listabulous", incompleteItemCount, listItemCount == 1 ? "": "s");
+        }
+        else
+        {
+            document.title = "Listabulous";
+        }
+    }
 }
