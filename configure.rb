651da8106d87372da778b36eba9df75a3673abe7
AndrewVos <andrew.vos@gmail.com>
2010-10-10 17:10:16 +0100

    Removed a useless comment

--- configure.rb
+++ configure.rb
@@ -4,8 +4,6 @@ end
 
 configure :production do
   ENV["SERVER_NAME"] = "listabulous.co.uk"
-  
-  # mongodb://app274790:7832nmwusv7of7nqr71yzu@flame.mongohq.com:27035/app274790
   mongohq_url = ENV['MONGOHQ_URL']
   MongoMapper.connection = Mongo::Connection.from_uri(mongohq_url)
   MongoMapper.database = mongohq_url[mongohq_url.rindex("/") + 1, mongohq_url.length]
